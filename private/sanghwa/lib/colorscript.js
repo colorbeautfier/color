f3dd4db14414ddda0b2140afc0f5f923fa21901a
sanghwa Jung <jjhangu@gmail.com>
2015-11-16 18:13:44 +0900

    hoho

--- private/sanghwa/lib/colorscript.js
+++ private/sanghwa/lib/colorscript.js
@@ -22,7 +22,8 @@
  * @type {{data: {}, init: Function, setLanguage: Function}}
  */
 var colorScripting = {
-    data : {},
+    data : { convertedObjects : [],
+        currentLogic : {} },
 
     init : function(name){
 
@@ -51,32 +52,95 @@ var colorScripting = {
 (function(){
    var javascript_sh =   function(){
 
-        this.content = "";
+        this.contents = [];
+       this.originalContent ="";
         this.changedcontent = "";
 
         this.sequenceLogic= [{
-           regExp : "",
-            color : "#0000"
+           regExp : /\/\*[^]*?\*\//g, // 대주석
+           color : "#ff00ff"
         },{
+            regExp : /\/\/.*/g, // 일반주석
+            color : "#ffff00"
+        },{
+            regExp : /".*?"|'.*?'/g, // string
+            color : "#f00000"
 
         },{
-
-        },{
-
+            regExp : /[0-9a-zA-Z]+/g, // 숫자나 글자인 부분
+            color : "#555000"
         }];
 
+       this.currentLogic = {};
+
+
         this.setContent = function(content){
+            this.contents[0] = content;
+            this.originalContent = content;
+        },
 
-        }
         this.execute = function(){
-            while(this.sequenceLogic.length>0){
-                var logic = this.sequenceLogic.shift();
-                var regExp = logic.regExp;
+            // 초기화
+            colorScripting.data.convertedObjects = [];
+            var todoContent = this.contents;
 
+            for(var i=0; i<this.sequenceLogic.length; i++){
+
+
+
+                this.currentLogic = this.sequenceLogic[i];
+                colorScripting.data.currentLogic = this.currentLogic;
+                var len = todoContent.length;
+                for(var j=0; j<len ;j++){
+
+                    var changedContent = todoContent[j].replace(this.currentLogic.regExp, this.parsingCallback);
+                }
+                // compare 링
+                colorScripting.data.convertedObjects.sort(this.compare);
+
+                // 태그와 태그 사이 컨텐츠 구분
+                var objs = colorScripting.data.convertedObjects;
+                todoContent = this.getContents(objs);
+
+                console.log(todoContent );
             }
             return this.changedcontent;
-        }
-    }
+        },
+            this.getContents = function(objs){
+                var contents = [];
+                var startIndex =0;
+                var endIndex =0;
+                for(var i=0; i<objs.length; i++){
+                    if(typeof objs[i].startIndex =='undefined'){
+                        continue;
+                    }
+                    if(i ==0 && objs[i].startIndex ==0){
+                        startIndex = objs[i].endIndex;
+                    }else{
+                         contents.push ( this.originalContent.substring(startIndex, objs[i].startIndex));
+                        startIndex = objs[i].endIndex;
+                    }
+                }
+                return contents;
+            }
+
+        this.compare = function (a,b) {
+           if (a.startIndex < b.startIndex)
+               return -1;
+           if (a.startIndex > b.startIndex)
+               return 1;
+           return 0;
+       }
+
+        this.parsingCallback = function (str, p1, p2, p3, p4,  offset, s){
+            var obj = {
+                startIndex : p1,
+                endIndex : p1+ str.length,
+                str :'<span style="color: '+ colorScripting.data.currentLogic.color +'">'+str + '<span>'
+            }
+            colorScripting.data.convertedObjects.push(obj);
+            return str;
+       }
+   }
     colorScripting.setLanguage("javascript", "sh", new javascript_sh());
 })();
-
