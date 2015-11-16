9b20b7e7ed0163b5da27131c7f33251685ebe6ed
sanghwa Jung <jjhangu@gmail.com>
2015-11-16 09:29:13 +0900

    hello

---
+++ private/sanghwa/lib/colorscript.js
@@ -0,0 +1,62 @@
+/**
+ * Created by ddavid on 2015-11-16.
+ */
+
+/**
+ *
+ * 예상 시나리오
+ * 1. init ( 랭귀지 찾기 )
+ * 2.
+ *
+ *
+ *
+ *
+ * 만들기 위해서는 다른사람들이 랭귀지를 셋팅할수있도록 해야한다.
+ *
+ * 크게 작업해야하는 사항
+ * 1. 주석
+ * 2. string값
+ * 3. 색깔체인지
+ * 4. esacpe 문자 변경
+ *
+ *
+ * @type {{data: {}, init: Function, setLanguage: Function}}
+ */
+var colorScripting = {
+    data : {},
+
+    init : function(name){
+
+    },
+
+    start : function(){
+
+    },
+
+    setLanguage : function(languageName, name, language){
+        this.languages[languageName.name = language];
+    },
+
+    languages : {}
+}
+
+
+(function(){
+   var javascript_sh =   function(){
+
+        this.content = "";
+        this.changedcontent = "";
+
+        this.sequenceGrammer = [{},{},{},{}];
+
+        this.setContent = function(content){
+
+        }
+        this.execute = function(){
+
+            return this.changedcontent;
+        }
+    }
+    colorScripting.setLanguage("javascript", "sh", javascript_sh);
+})();
+
