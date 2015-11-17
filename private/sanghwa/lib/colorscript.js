aec73a52e6dbca6f546ad0e4ddaf16130ff4a7f4
sanghwa Jung <jjhangu@gmail.com>
2015-11-17 10:26:00 +0900

    structure

--- private/sanghwa/lib/colorscript.js
+++ private/sanghwa/lib/colorscript.js
@@ -3,34 +3,28 @@
  */
 /**
  *
- * 예상 시나리오
- * 1. init ( 랭귀지 찾기 )
- * 2.
- *
- *
- *
- *
- * 만들기 위해서는 다른사람들이 랭귀지를 셋팅할수있도록 해야한다.
- *
- * 크게 작업해야하는 사항
- * 1. 주석
- * 2. string값
- * 3. 색깔체인지
- * 4. esacpe 문자 변경
- *
  *
  * @type {{data: {}, init: Function, setLanguage: Function}}
  */
 var colorScripting = {
     data : { convertedObjects : [],
-        currentLogic : {} },
+        currentLogic : {},
+    startIndex :0},
 
     init : function(name){
 
     },
 
     start : function(){
+        return this.controller.execute();
+    },
 
+    setContent : function(content){
+        this.controller.setContent(content);
+    },
+
+    initLanguage : function(languageName, name){
+        this.controller.sequenceLogic = this.languages[languageName][name].sequenceLogic;
     },
 
     setLanguage : function(languageName, name, language){
@@ -42,19 +36,18 @@ var colorScripting = {
         this.languages[languageName][name]  = language;
     },
 
-    getLanguage : function(languageName, name){
-        return this.languages[languageName][name];
-    },
+    languages : {},
 
-    languages : {}
+    controller : null
 };
 
 (function(){
-   var javascript_sh =   function(){
+    /**
+     * 언어 셋팅
+     */
 
-        this.contents = [];
-       this.originalContent ="";
-        this.changedcontent = "";
+        // javascript
+   var javascript_sh =   function(){
 
         this.sequenceLogic= [{
            regExp : /\/\*[^]*?\*\//g, // 대주석
@@ -70,77 +63,128 @@ var colorScripting = {
             regExp : /[0-9a-zA-Z]+/g, // 숫자나 글자인 부분
             color : "#555000"
         }];
+   }
 
-       this.currentLogic = {};
+    // controller 이거는 사용안함
+    var controller = function(){
+        this.contents = [];
+        this.originalContent ="";
+        this.changedcontent = "";
+
+        this.sequenceLogic="";
+
+        this.currentLogic = {};
 
 
         this.setContent = function(content){
-            this.contents[0] = content;
+            this.contents = [{startIndex : 0, content: content}];
             this.originalContent = content;
         },
 
-        this.execute = function(){
-            // 초기화
-            colorScripting.data.convertedObjects = [];
-            var todoContent = this.contents;
+            this.execute = function(){
+                // 초기화
+                colorScripting.data.convertedObjects = [];
+                colorScripting.data.startIndex =0;
+                var todoContent = this.contents;
 
-            for(var i=0; i<this.sequenceLogic.length; i++){
+                console.log(this.sequenceLogic);
+                console.log(this.sequenceLogic.length);
+                for(var i=0; i<this.sequenceLogic.length; i++){
+                    this.currentLogic = this.sequenceLogic[i];
+                    colorScripting.data.currentLogic = this.currentLogic;
 
+                    var len = todoContent.length;
+                    for(var j=0; j<len ;j++){
+                        colorScripting.data.startIndex =todoContent[j].startIndex;
+                        var changedContent = todoContent[j].content.replace(this.currentLogic.regExp, this.parsingCallback);
+                    }
+                    // compare 링
+                    colorScripting.data.convertedObjects.sort(this.compare);
 
+                    // 태그와 태그 사이 컨텐츠 구분
+                    var objs = colorScripting.data.convertedObjects;
+                    todoContent = this.getContents(objs);
 
-                this.currentLogic = this.sequenceLogic[i];
-                colorScripting.data.currentLogic = this.currentLogic;
-                var len = todoContent.length;
-                for(var j=0; j<len ;j++){
-
-                    var changedContent = todoContent[j].replace(this.currentLogic.regExp, this.parsingCallback);
+                    console.log(todoContent );
                 }
-                // compare 링
-                colorScripting.data.convertedObjects.sort(this.compare);
 
-                // 태그와 태그 사이 컨텐츠 구분
+                this.changedcontent = this.convertedContent();
+                console.log(this.changedcontent);
+
+                return this.changedcontent;
+            },
+
+            this.convertedContent =function(){
                 var objs = colorScripting.data.convertedObjects;
-                todoContent = this.getContents(objs);
-
-                console.log(todoContent );
-            }
-            return this.changedcontent;
-        },
-            this.getContents = function(objs){
-                var contents = [];
                 var startIndex =0;
-                var endIndex =0;
+                var convertedContent = "";
+
                 for(var i=0; i<objs.length; i++){
                     if(typeof objs[i].startIndex =='undefined'){
                         continue;
                     }
-                    if(i ==0 && objs[i].startIndex ==0){
-                        startIndex = objs[i].endIndex;
+                    if(i ==0 ){
+                        if(objs[i].startIndex ==0){  // 시작일때
+                            convertedContent += this.originalContent.substring(startIndex , objs[i].endIndex);
+                        }else{
+                            convertedContent += this.originalContent.substring(0, objs[i].startIndex);
+                            convertedContent += objs[i].str;
+                        }
                     }else{
-                         contents.push ( this.originalContent.substring(startIndex, objs[i].startIndex));
-                        startIndex = objs[i].endIndex;
+                        convertedContent += this.originalContent.substring(startIndex , objs[i].startIndex);
+                        convertedContent += objs[i].str;
                     }
+                    startIndex = objs[i].endIndex;
+
+                    if(i==objs.length-1){
+                        convertedContent += this.originalContent.substring(objs[i].endIndex , this.originalContent.length);
+                    }
+
                 }
-                return contents;
+                return convertedContent;
+
             }
 
+        this.getContents = function(objs){
+            var contents = [];
+            var startIndex =0;
+            var endIndex =0;
+            for(var i=0; i<objs.length; i++){
+                if(typeof objs[i].startIndex =='undefined'){
+                    continue;
+                }
+                if(i ==0 && objs[i].startIndex ==0){
+                    startIndex = objs[i].endIndex;
+                }else if(i==objs.length-1){
+                    contents.push ( {startIndex :  objs[i].endIndex, 'content' : this.originalContent.substring(objs[i].endIndex, this.originalContent.length)});
+                }
+                else{
+                    contents.push ( {startIndex : startIndex, 'content' : this.originalContent.substring(startIndex, objs[i].startIndex)});
+                    startIndex = objs[i].endIndex;
+                }
+            }
+            return contents;
+        }
+
         this.compare = function (a,b) {
-           if (a.startIndex < b.startIndex)
-               return -1;
-           if (a.startIndex > b.startIndex)
-               return 1;
-           return 0;
-       }
+            if (a.startIndex < b.startIndex)
+                return -1;
+            if (a.startIndex > b.startIndex)
+                return 1;
+            return 0;
+        }
 
         this.parsingCallback = function (str, p1, p2, p3, p4,  offset, s){
             var obj = {
-                startIndex : p1,
-                endIndex : p1+ str.length,
-                str :'<span style="color: '+ colorScripting.data.currentLogic.color +'">'+str + '<span>'
+                startIndex : p1 +  colorScripting.data.startIndex,
+                endIndex : p1+ str.length +  colorScripting.data.startIndex,
+                str :'<span style="color: '+ colorScripting.data.currentLogic.color +'">'+str + '</span>'
             }
             colorScripting.data.convertedObjects.push(obj);
             return str;
-       }
-   }
+        }
+    }
+
     colorScripting.setLanguage("javascript", "sh", new javascript_sh());
+    colorScripting.controller =new controller();
 })();
